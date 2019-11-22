542d6bc314836141188aea3d5f1040876791e971
Reacoder <sampythoner@gmail.com>
2019-11-22 10:01:35 +0800

    [Test] 试一试。

--- app/src/main/java/com/zhao/androidexplore/MainActivity.kt
+++ app/src/main/java/com/zhao/androidexplore/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.zhao.androidexplore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //test git
         setContentView(R.layout.activity_main)
     }
 }
